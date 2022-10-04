f6cb004520f01de7a0d270271c451b896a22d35b
Xway0312 <106440942+Xway0312@users.noreply.github.com>
2022-10-04 18:54:49 +0800

    Update _isAdmin.ts

--- pages/api/utils/_isAdmin.ts
+++ pages/api/utils/_isAdmin.ts
@@ -1,15 +1,15 @@
-import { Session } from "@auth0/nextjs-auth0";
-import { ManagementClient } from "auth0";
-
-const isAdmin = async (session: Session) => {
-    const client = new ManagementClient({
-        domain: 'newhongqi.us.auth0.com',
-        clientId: process.env.AUTH0_CLIENT_ID as string,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
-        scope: 'read:users read:users_app_metadata',
-    })
-    const userInfo = await client.getUser({ id: session.user.sub });
-    return Boolean(userInfo.app_metadata?.admin);
-}
-
+import { Session } from "@auth0/nextjs-auth0"; 
+import { ManagementClient } from "auth0"; 
+ 
+const isAdmin = async (session: Session) => { 
+    const client = new ManagementClient({ 
+        domain: (process.env.AUTH0_ISSUER_BASE_URL as string).split('/').find(token => token.includes('com')) as string, 
+        clientId: process.env.AUTH0_CLIENT_ID as string, 
+        clientSecret: process.env.AUTH0_CLIENT_SECRET as string, 
+        scope: 'read:users read:users_app_metadata', 
+    }) 
+    const userInfo = await client.getUser({ id: session.user.sub }); 
+    return Boolean(userInfo.app_metadata?.admin); 
+} 
+ 
 export default isAdmin;
